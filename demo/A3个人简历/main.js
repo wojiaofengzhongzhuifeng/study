45f160cbff3eabd5000d81dda471be47d304975f
raojiajun <471412450@qq.com>
2017-11-30 10:36:32 +0800

    添加一个改的

--- demo/A3个人简历/main.js
+++ demo/A3个人简历/main.js
@@ -1,3 +1,14 @@
+// 加载动画
 window.onload = function(){
     welcome.classList.add("active")
 }
+// 监听页面的滑动
+window.onscroll = function(x){
+    var scrollHeight = window.scrollY
+    if(scrollHeight === 0){
+        navCt1.classList.remove("active")
+    } else {
+        navCt1.classList.add("active")
+    }
+}
+
